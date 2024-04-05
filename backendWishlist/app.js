f176ce6235ca1729ec1c0d3f86abacc3c571810c
Nerea <neresaas@gmail.com>
2024-04-05 18:15:06 +0200

    middleware

--- backendWishlist/app.js
+++ backendWishlist/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+let activeApiKeys = require("./activeApiKeys");
+
 const port = 4000;
 
 const app = express();
@@ -7,6 +10,33 @@ app.use(express.json());
 let routerUsers = require("./routers/routerUsers");
 let routerPresents = require("./routers/routerPresents");
 let routerFriends = require("./routers/routerFriends");
+
+app.use(["/presents", "/friends"] ,(req, res, next)=>{
+	console.log("Middleware execution")
+
+    let apiKey = req.query.apiKey
+    
+    if(apiKey == undefined) {
+        return res.status(401).json({error: "No apiKey"})
+    }
+
+    let infoApiKey = []
+
+    try {
+        infoApiKey = jwt.verify(apiKey, "secret")
+
+    } catch (e) {
+        return res.status(401).json({error: "Invalid apiKey"})
+    }
+
+    if(infoApiKey == undefined || activeApiKeys.indexOf(apiKey) == -1) {
+        return res.status(401).json({error: "Invalid apiKey"})
+    }
+
+    req.infoApiKey = infoApiKey;
+
+    next()
+})
 
 app.use("/users", routerUsers);
 app.use("/presents", routerPresents);
