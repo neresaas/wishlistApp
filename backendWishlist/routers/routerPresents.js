e9286d821a21408a1956bc28eb0b600785e72da8
Nerea <neresaas@gmail.com>
2024-04-06 18:06:32 +0200

    PUT /presents/:id

--- backendWishlist/routers/routerPresents.js
+++ backendWishlist/routers/routerPresents.js
@@ -76,7 +76,7 @@ routerPresents.post("/", async (req, res) => {
         return res.status(400).json({errors: errors})
     }
 
-    database.connect()
+    database.connect();
 
     let insertedPresent = []
 
@@ -84,13 +84,65 @@ routerPresents.post("/", async (req, res) => {
         insertedPresent = await database.query("INSERT INTO presents (userId, name, description, url, price) VALUES (?, ?, ?, ?, ?)", [req.infoApiKey.id, name, description, url, price])
 
     } catch(e) {
-        database.disConnect()
+        database.disConnect();
+
         return res.status(400).json({errors: "Error"})
+    }
+
+    database.disConnect();
+
+    res.json({inserted: insertedPresent})
+});
+
+routerPresents.put("/:id", async (req, res) => {
+    let id = req.params.id
+    let name = req.body.name
+    let description = req.body.description
+    let url = req.body.url
+    let price = req.body.price
+
+    let errors = []
+
+    if ( name == undefined ) {
+        errors.push("No name in body")
+    }
+
+    if ( description == undefined ) {
+        errors.push("No description in body")
+    }
+  
+    if ( url == undefined ) {
+        errors.push("No URL in body")
+    }
+  
+    if (isNaN(price)) {
+        errors.push("Price is not a number")
+    }
+  
+    if (parseFloat(price) <= 0) {
+        errors.push("Price must be higher than 0")
+    }
+  
+    if (errors.length > 0) {
+        return res.status(400).json({errors: errors})
+    }
+
+    database.connect();
+    
+    let updatedPresent = []
+
+    try {
+        updatedPresent = await database.query("UPDATE presents SET name = ?, description = ?, url = ?, price = ? WHERE id = ? AND userId = ?", [name, description, url, price, id, req.infoApiKey.id])
+
+    } catch (e) {
+        database.disConnect()
+
+        return res.status(400).json({errors: "Error in updated presents"})
     }
 
     database.disConnect()
 
-    res.json({inserted: insertedPresent})
+    res.json({modified: updatedPresent})
 });
 
 module.exports = routerPresents;
